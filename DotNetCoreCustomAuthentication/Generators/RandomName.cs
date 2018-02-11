bfed04b97fe400c0859bf5d125f2fc5bcf9510b9
dbl4k <github@dbl4k.net>
2018-02-11 10:49:27 +0000

    add random name generator

---
+++ DotNetCoreCustomAuthentication/Generators/RandomName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCustomAuthentication.Generators
+{
+    public static class RandomName
+    {
+        private readonly static Random R = new Random();
+
+        private readonly static string[] FirstNames = { "Boris", "Charles", "Gary", "David", "Tommy", "Mike" };
+        private readonly static string[] LastNames = { "Jones", "Smith", "Lewis", "Lloyd", "Davies", "Markowicz" };
+
+        public static string GetFirstName()
+        {
+            return FirstNames[R.Next(FirstNames.Length)];
+        }
+
+        public static string GetLastName()
+        {
+            return LastNames[R.Next(LastNames.Length)];
+        }
+
+    }
+}
